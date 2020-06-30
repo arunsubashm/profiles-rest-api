e2c7aa795d9bb311409f0299cdc76c7172cc9acb
M Arun Subash <arunsubashm@gmail.com>
2020-06-30 19:30:26 +0530

    Allow hosts checkin

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '-8(1ot)iwrqb0tthcjsm8$w&w8l#uu(bv&r*qcqsro(by(jw7&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-35-178-211-40.eu-west-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
